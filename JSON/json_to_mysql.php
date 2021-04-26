3e832e0ca2fc1410ef347834f532b5518ba542aa
tashauliak <55832873+tashauliak@users.noreply.github.com>
2021-04-26 07:17:40 +0700

    Update json_to_mysql.php

--- JSON/json_to_mysql.php
+++ JSON/json_to_mysql.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>  
       <head>  
-           <title>Webslesson Tutorial</title> 
+           <title></title> 
            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
